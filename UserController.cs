474bdd0f9f656098939858677c0de1914dc5ab7c
github_cloudlabsuser_2041 <github_cloudlabsuser_2041@cloudlabsaiuser.com>
2024-11-20 17:07:26 +0000

    Add UserController with CRUD actions and update assembly info

---
+++ UserController.cs
@@ -0,0 +1,101 @@
+using Microsoft.AspNetCore.Mvc;
+using MyMvcApp.Models; // Add this line if User class is in Models namespace
+
+
+public class User
+
+{
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Phone { get; set; } // Add this line
+
+}
+public class UserController : Controller
+{
+    private List<User> userlist = new List<User>();
+
+    public ActionResult Index()
+    {
+        return View(userlist);
+    }
+
+    // GET: User/Edit/5
+    public ActionResult Edit(int id)
+    {
+        var user = userlist.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return View(user);
+    }
+
+    // POST: User/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, User user)
+    {
+        if (id != user.Id)
+        {
+            return BadRequest();
+        }
+
+        if (ModelState.IsValid)
+        {
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser != null)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                existingUser.Phone = user.Phone;
+                return RedirectToAction(nameof(Index));
+            }
+            return NotFound();
+        }
+        return View(user);
+    }
+
+    // GET: User/Delete/5
+    public ActionResult Delete(int id)
+    {
+        var user = userlist.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return View(user);
+    }
+
+    // POST: User/Delete/5
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var user = userlist.FirstOrDefault(u => u.Id == id);
+        if (user != null)
+        {
+            userlist.Remove(user);
+            return RedirectToAction(nameof(Index));
+        }
+        return NotFound();
+    }
+
+    // GET: User/Details/5
+    public ActionResult Details(int id)
+    {
+        var user = userlist.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return View(user);
+    }
+}
+
+  git config --global user.email "github_cloudlabsuser_2041@cloudlabsaiuser.com"
+  git config --global user.name "github_cloudlabsuser_2041"
